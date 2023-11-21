5874d42e073108e3a520c8ff2d2a6e95ba2a3c7b
h-hosoda-ml <h.hosoda.ml@gmail.com>
2023-11-21 17:16:44 +0900

    add: configにパスを追加

--- sony_20231120/src/conf/config.py
+++ sony_20231120/src/conf/config.py
@@ -6,4 +6,7 @@ load_dotenv()
 
 # srcまでのパスを格納
 BASE_PATH: str = os.getenv("BASE_PATH", "")
+YOLO_BASE: str = os.getenv("YOLO_BASE", "")
+MODEL_PATH: str = os.getenv("MODEL_PATH", "")
 DATASET_PATH: str = os.path.join(BASE_PATH, "datasets")
+YOLO_DATASET_PATH: str = os.path.join(YOLO_BASE, "dataset")
