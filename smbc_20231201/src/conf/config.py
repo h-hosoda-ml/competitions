120f5c822251586476551d34a6a06693b9e9edca
h-hosoda-ml <h.hosoda.ml@gmail.com>
2023-12-02 11:31:58 +0900

    add: config.pyの作成

---
+++ smbc_20231201/src/conf/config.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+DATA_PATH = os.path.join(BASE_PATH, "data")
